713b118adbdee0bbecc9ec1af5bdd6a04ce9b119
Ritesh Satapathy <ritesh@Riteshs-MacBook-Air.local>
2026-08-01 20:10:21 +0530

    code review comments implemented

--- projects/day2/calculator.py
+++ projects/day2/calculator.py
@@ -1,20 +1,20 @@
 from logger import logger
 
-def add(a, b):
-    logger.info(f"Adding {a} and {b}") 
+def add(a:float, b:float) -> float:
+    #logger.info(f"Adding {a} and {b}") 
     return a + b
 
-def subtract(a, b):
-    logger.info(f"Subtracting {b} from {a}")
+def subtract(a:float, b:float) -> float:
+    #logger.info(f"Subtracting {b} from {a}")
     return a - b
 
-def multiply(a, b):
-    logger.info(f"Multiplying {a} and {b}")
+def multiply(a:float, b:float) -> float:
+    #logger.info(f"Multiplying {a} and {b}")
     return a * b
 
-def divide(a, b):
-    logger.info(f"Dividing {a} by {b}")
+def divide(a:float, b:float) -> float:
+    #logger.info(f"Dividing {a} by {b}")
     if b == 0:
         logger.error("Attempted to divide by zero.")
-        #raise ValueError("Cannot divide by zero.")
+        raise ValueError("Cannot divide by zero.")
     return a / b
